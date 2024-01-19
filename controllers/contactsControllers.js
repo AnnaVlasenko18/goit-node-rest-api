7af284954f9bf566c0b206e8acb59690bd2e96f0
AnnaVlasenko18 <131859714+AnnaVlasenko18@users.noreply.github.com>
2024-01-19 20:59:55 +0200

    task_end

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,87 @@
-import contactsService from "../services/contactsServices.js";
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  updateById,
+  removeContact,
+} = require("../services/contactsServices.js");
 
-export const getAllContacts = (req, res) => {};
+const { HttpError } = require("../helpers/HttpError.js");
 
-export const getContactById = (req, res) => {};
+const {
+  createContactSchema,
+  updateContactSchema,
+} = require("../schemas/contactsSchemas");
 
-export const deleteContact = (req, res) => {};
+const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await getContactById(id);
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  try {
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "Body must have at least one field");
+    }
+    const { id } = req.params;
+    const result = await updateById(id, req.body);
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const removedContact = await removeContact(id);
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+    res.json({ message: "Delete success" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,54 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "../db/contacts.json");
+
+async function listContacts() {
+  const allContacts = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(allContacts);
+}
+
+async function getContactById(id) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === id);
+  return result || null;
+}
+
+async function addContact(name, email, phone) {
+  const contact = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  contact.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
+  return newContact;
+}
+
+async function updateById(id, body) {
+  const contact = await listContacts();
+  const index = contact.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contact[index] = { ...contact[index], ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
+  return contact[index];
+}
+
+async function removeContact(contactId) {
+  const contact = await listContacts();
+  const index = contact.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [removedContact] = contact.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
+  return removedContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  updateById,
+  removeContact,
+};
