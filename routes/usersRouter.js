ce8f3fbdd8c5e068a4533eb1d398cb1287282751
AnnaVlasenko18 <131859714+AnnaVlasenko18@users.noreply.github.com>
2024-02-09 18:22:15 +0200

    task_end

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,12 +5,22 @@ const {
   login,
   getCurrent,
   logout,
+  updateAvatar,
 } = require("../controllers/userControllers.js");
 const { authenticate } = require("../helpers/authenticate");
+const { upload } = require("../helpers/upload.js");
+const { sizeChange } = require("../helpers/sizeChange.js");
 
 usersRouter.post("/register", register);
 usersRouter.post("/login", login);
 usersRouter.get("/current", authenticate, getCurrent);
 usersRouter.post("/logout", authenticate, logout);
+usersRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  updateAvatar,
+  sizeChange
+);
 
 module.exports = usersRouter;
